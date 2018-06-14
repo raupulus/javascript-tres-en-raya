5e54cfc70c1524f825859b6b949390b0336a26a1
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-14 17:16:51 +0200

    Creo clase para fichas de tipo equis

---
+++ assets/js/class/Equis.js
@@ -0,0 +1,16 @@
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2018 Raúl Caro Pastorino
+ * @license https://wwww.gnu.org/licenses/gpl.txt
+ **/
+
+class Equis extends BaseFicha {
+    constructor(color) {
+        super(color);
+        this._simbolo = 'X';
+    }
+
+    get simbolo() {
+        return this._simbolo;
+    }
+}
