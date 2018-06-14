92a143c222de256aef95b62ad5b993e760091911
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-15 00:48:26 +0200

    Añado función de ejemplo de tirada

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,19 @@
  * @copyright Copyright © 2018 Raúl Caro Pastorino
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
+
+
+/**
+  * Tirada demostrativa en Diagonal. Comenzando las ficha 'X'
+  */
+function tiradaDemostrativa() {
+    var tablero = new Tablero();
+    tablero.nuevaJugada(0,0);
+    tablero.nuevaJugada(0,1);
+    tablero.nuevaJugada(1,1);
+    tablero.nuevaJugada(0,2);
+    tablero.nuevaJugada(2,2);
+    tablero.nuevaJugada(1,2);
+}
+
+tiradaDemostrativa();
