b72aaf529474212c3b125188821fa9148b435afc
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-14 17:15:57 +0200

    Creo clase base para las fichas

---
+++ assets/js/class/BaseFicha.js
@@ -0,0 +1,17 @@
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2018 Raúl Caro Pastorino
+ * @license https://wwww.gnu.org/licenses/gpl.txt
+**/
+
+// class {}
+
+class BaseFicha {
+    constructor(inColor) {
+        this._color = inColor;
+    }
+
+    get color() {
+        return this._color;
+    }
+}
