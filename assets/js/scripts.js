c1076fe9f7cce920fea77568dff1dacbb82b5978
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-15 09:57:52 +0200

    Genero lógica de funciones

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,18 +4,38 @@
  * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
 
+var tablero;
 
 /**
   * Tirada demostrativa en Diagonal. Comenzando las ficha 'X'
   */
-function tiradaDemostrativa() {
+function tiradaDemo() {
     var tablero = new Tablero();
-    tablero.nuevaJugada(0,0);
-    tablero.nuevaJugada(0,1);
-    tablero.nuevaJugada(1,1);
-    tablero.nuevaJugada(0,2);
-    tablero.nuevaJugada(2,2);
-    tablero.nuevaJugada(1,2);
+    tablero.nuevaJugada(0,0);  // Jugador X
+    tablero.nuevaJugada(0,1);  // Jugador Y
+    tablero.nuevaJugada(1,1);  // Jugador X
+    tablero.nuevaJugada(0,2);  // Jugador Y
+    tablero.nuevaJugada(2,2);  // Jugador X
+}
+//tiradaDemo();
+
+function resetGame() {
+    
 }
 
-tiradaDemostrativa();
+function showInfo() {
+
+}
+
+function addEvents() {
+
+}
+
+function createGame(size) {
+    tablero = new Tablero(size, 'X');
+}
+
+function init() {
+    createGame(3);
+    addEvents();
+}
